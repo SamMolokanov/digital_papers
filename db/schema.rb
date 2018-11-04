926f20c10afe54f2e5082d7da31a98f29301360c
Semjon Molokanov <molokanovcv@yandex.com>
2018-11-04 14:40:03 +0100

    Install Active Storage
      * Using uuid as PK and FK

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_184722) do
+ActiveRecord::Schema.define(version: 2018_11_04_133128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.uuid "record_id", null: false
+    t.uuid "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "name", limit: 255
@@ -28,7 +49,7 @@ ActiveRecord::Schema.define(version: 2018_10_15_184722) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["sessions"], name: "index_users_on_sessions_jsonb_path_ops", opclass: :jsonb_path_ops, using: :gin
+    t.index ["sessions"], name: "index_users_on_sessions", opclass: :jsonb_path_ops, using: :gin
   end
 
 end
